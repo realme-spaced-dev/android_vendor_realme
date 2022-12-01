433017bfc45787623bf3615491838a5d8ab3f333
rk134 <spaced@rk134.cf>
2022-12-01 14:50:31 +0000

    spaced-ims: Import from RMX3151_11_C.13
    Signed-off-by: rk134 <spaced@rk134.cf>

--- spaced-ims/spaced-ims-vendor.mk
+++ spaced-ims/spaced-ims-vendor.mk
@@ -17,3 +17,13 @@ PRODUCT_PACKAGES += \
     mediatek-telecom-common \
     mediatek-telephony-base \
     mediatek-telephony-common
+
+# Radio IMS Boot Jar
+PRODUCT_BOOT_JARS += \
+    mediatek-common \
+    mediatek-framework \
+    mediatek-ims-base \
+    mediatek-ims-common \
+    mediatek-telecom-common \
+    mediatek-telephony-base \
+    mediatek-telephony-common
